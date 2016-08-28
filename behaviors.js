3167471edd6cb7e4a01a1b2ca0e7cec92204ad40
erik <erik@unswornindustries.com>
2016-08-28 18:54:19 +0200

    Name behaviors like this: 001 ... 100

--- behaviors.js
+++ behaviors.js
@@ -1,11 +1,11 @@
-var behaviors = [
-  'crazy',
-  'tired',
-  'calm'
-];
+const MAX_BEHAVIOR = 100;
 
 module.exports = {
+  // Return a random string '000', to '100'
   getRandom: function() {
-    return behaviors[Math.floor(Math.random() * behaviors.length)];
+    const num = Math.floor(Math.random() * MAX_BEHAVIOR);
+    const paddedStr = (num < 10 ? '00' : num < 100 ? '0' : '') + num;  
+    return paddedStr;
   }
 };
+
